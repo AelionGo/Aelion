94fc71605f0ddd38b2a412723920d3cdacfd741a
FoyonaCZY <czydyx1@126.com>
2025-06-23 02:00:54 +0800

    initial commit

---
+++ internal/router/handler/v1/ping/ping.go
@@ -0,0 +1,26 @@
+// Filename: ping.go
+// Description: 心跳
+
+package ping
+
+import (
+	"context"
+	logic "github.com/AelionGo/Aelion/internal/logic/ping"
+	"github.com/AelionGo/Aelion/internal/svc"
+	"github.com/AelionGo/Aelion/internal/types"
+	"github.com/AelionGo/Aelion/pkg/errors"
+	"github.com/AelionGo/Aelion/pkg/msg"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+func PingHandler(svcCtx *svc.ServiceContext) app.HandlerFunc {
+	return func(c context.Context, ctx *app.RequestContext) {
+		req := &types.PingRequest{}
+
+		l := logic.NewPingLogic(ctx, svcCtx)
+		resp, _ := l.Ping(req)
+		response := msg.GetResponse(errors.OK, resp)
+		ctx.JSON(consts.StatusOK, response)
+	}
+}
---
+++ internal/svc/ServiceContext.go
@@ -0,0 +1,20 @@
+// package svc
+
+// Filename: ServiceContext.go
+// Description: 服务上下文
+
+package svc
+
+import (
+	"github.com/AelionGo/Aelion/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
